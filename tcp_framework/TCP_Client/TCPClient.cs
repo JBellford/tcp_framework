78a5ecaddda733dd027dc2782467dfe9b0985cde
Jordan <Jordan>
2021-11-11 02:46:36 +0000

    Client

--- tcp_framework/TCP_Client/TCPClient.cs
+++ tcp_framework/TCP_Client/TCPClient.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
 
 namespace tcp_framework.TCP_Client
 {
     public class TCPClient
     {
 
-        public TCPClient(string ip, int port, int timeout)
+        public TCPClient()
         {
-
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
-        public void Connect()
-        {
-
-        }
     }
 }
--- tcp_framework/TCP_Client/TCPClient_Data.cs
+++ tcp_framework/TCP_Client/TCPClient_Data.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace tcp_framework.TCP_Client
 {
     public class TCPClient_Data
     {
+        private Socket _clientSocket;
+        private string _serverIP;
+        private int _serverPort;
+
+        private double _totalBytesSent;
+        private double _totalByteReceived;
+
+        public TCPClient_Data(string serverIP, int serverPort)
+        {
+
+        }
+
+        public string ServerIP
+        {
+            get
+            {
+                return _serverIP;
+            }
+            set
+            {
+                _serverIP = value;
+            }
+        }
+        public int ServerPort
+        {
+            get
+            {
+                return _serverPort;
+            }
+            set
+            {
+                _serverPort = value;
+            }
+        }
+
+    
+
     }
 }
